c5ea6add8e0204b1c25c13015d4d2937eaa1155b
tamamu <tamamu.1r1s@gmail.com>
2017-12-07 11:59:50 +0900

    Solve ABC040-C

--- atcoder-abc040c.lisp
+++ atcoder-abc040c.lisp
@@ -1,20 +1,18 @@
-(defun bfs (arr len idx acc)
-  (cond
-    ((= idx (1- len)) acc)
-    ((= idx (- len 2))
-     (bfs arr len (1+ idx) (+ acc
-			      (abs (- (aref arr idx)
-				      (aref arr (1+ idx)))))))
-    (t (min (bfs arr len (1+ idx) (+ acc
-				    (abs (- (aref arr idx)
-					    (aref arr (1+ idx))))))
-	    (bfs arr len (+ 2 idx) (+ acc
-				      (abs (- (aref arr idx)
-					      (aref arr (+ 2 idx))))))))))
+(defun dp (n arr)
+  (let* ((cost (make-array n :initial-element 0)))
+    (setf (aref cost 1)
+	  (abs (- (aref arr 0) (aref arr 1))))
+    (loop for i from 2 below n do
+	  (setf (aref cost i)
+		(min (+ (abs (- (aref arr i) (aref arr (1- i))))
+			(aref cost (1- i)))
+		     (+ (abs (- (aref arr i) (aref arr (- i 2))))
+			(aref cost (- i 2))))))
+    (aref cost (1- n))))
 
 (let* ((n (read))
        (a (make-array n)))
   (dotimes (i n)
     (setf (aref a i) (read)))
-  (format t "~A~%" (bfs a (length a) 0 0)))
+  (format t "~A~%" (dp n a)))
 
